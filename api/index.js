b318b8fba52b3cc17dd0c4b524a9a5284580ba83
ThomasBackend <thomasmj.mt@gmail.com>
2024-08-11 14:47:36 +0100

    Body Parser Setup Removal

--- api/index.js
+++ api/index.js
@@ -7,11 +7,6 @@ require('dotenv').config();
 const app = express();
 
 app.use(express.json());
-// app.use(bodyParser.urlencoded({
-//     extended: true
-// }));
-
-// app.use(bodyParser.json());
 
 
 app.use('/article', articleRouter)
